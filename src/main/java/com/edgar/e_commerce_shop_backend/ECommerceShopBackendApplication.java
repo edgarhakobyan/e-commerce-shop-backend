f405aee97719c6e3334b3bd67ba6f6ad213a8eee
Edgar Hakobyan <hakobyanedgar24@gmail.com>
2024-06-07 00:53:47 +0400

    Initial app

---
+++ src/main/java/com/edgar/e_commerce_shop_backend/ECommerceShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edgar.e_commerce_shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgar/e_commerce_shop_backend/ECommerceShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgar.e_commerce_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
