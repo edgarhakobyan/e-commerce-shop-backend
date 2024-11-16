5dbbb8fc0b51a7de831f63d60ce5ccf336837d1d
Edgar Hakobyan <hakobyanedgar24@gmail.com>
2024-11-17 01:54:40 +0400

    pushed changes

--- src/main/java/com/edgar/e_commerce_shop_backend/security/WebSecurityConfig.java
+++ src/main/java/com/edgar/e_commerce_shop_backend/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.edgar.e_commerce_shop_backend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
@@ -16,12 +17,17 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable();
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable);
+
         http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
-        http.authorizeHttpRequests()
+
+        http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/api/product", "/api/auth/register", "/api/auth/login", "/api/auth/verify",
                         "/api/auth/forgot", "/api/auth/reset").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+        );
+
         return http.build();
     }
 }
